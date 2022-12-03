f82e33e814edcb2ad9775e51e67c426c4b585470
Erik Neemann <eneemann@utah.gov>
2022-12-03 10:32:21 -0700

    add: animation and daylight time

--- mosaic_rasters_wbt.py
+++ mosaic_rasters_wbt.py
@@ -22,15 +22,15 @@ wbt.set_verbose_mode(True)
 wbt.set_compress_rasters(True)
 
 # Define variables
-data_dir = r"C:\Users\eneemann\Desktop\Bountiful Lidar"
+data_dir = r"C:\Users\eneemann\Desktop\Zion Lidar"
 os.chdir(data_dir)
 wbt.set_working_dir(data_dir)
 
-outfile = 'hh_all.tif'
+outfile = 'Zion_all.tif'
 
 file_list = []
 for file in os.listdir(data_dir):
-    if 'hh' in file and file.endswith('.tif'):
+    if 'NED' in file and file.endswith('.img'):
         file_list.append(file)
 
         
@@ -51,3 +51,38 @@ print("Script shutting down ...")
 readable_end = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 print("The script end time is {}".format(readable_end))
 print("Time elapsed: {:.2f}s".format(time.time() - start_time))
+
+
+## Need license to use
+#wbt.shadow_animation(
+#    "clipped_DSM.tif", 
+#    'shadows.html', 
+#    palette="atlas", 
+#    max_dist="", 
+#    date="21/06/2021", 
+#    interval=15, 
+#    location="40.865459/-111.873929/-6", 
+#    height=600, 
+#    delay=250, 
+#    label="")
+
+
+#wbt.horizon_angle(
+#    "clipped_DSM.tif", 
+#    'horizon_angle.tif', 
+#    azimuth=180.0, 
+#    max_dist=100.0)
+#
+#wbt.time_in_daylight(
+#    "clipped_DSM.tif", 
+#    'time_in_daylight.tif', 
+#    40.865459, 
+#    -111.873929, 
+#    az_fraction=10.0, 
+#    max_dist=100.0, 
+#    utc_offset="-06:00", 
+#    start_day=1, 
+#    end_day=365, 
+#    start_time="00:00:00", 
+#    end_time="23:59:59")
+
